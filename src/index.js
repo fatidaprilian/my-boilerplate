dc53198e4317161a1ad7f96ef66944d844774ebc
fatidaprilian <64300224+fatidaprilian@users.noreply.github.com>
2024-02-24 19:24:57 +0700

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// File: src/index.js
+
+import "./styles.css";
+
+const element = document.getElementById('app');  
+
+element.innerHTML = `
+  <div>
+    <h1>Hello World!</h1>
+
+    <p>Welcome to my boilerplate web app</p>
+  </div>
+`;
